394df1888113345ec7b06ac1359bd0d286e23cb0
Stephen-Hallett <stevohallett@gmail.com>
2025-06-02 17:41:41 +1200

    fix: Try stabilise networking

--- Hosts/Homelab/hardware-configuration.nix
+++ Hosts/Homelab/hardware-configuration.nix
@@ -54,8 +54,8 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp34s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp37s0.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp34s0.useDHCP = lib.mkDefault false;
+  networking.interfaces.wlp37s0.useDHCP = lib.mkDefault true;
 
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
--- NixModules/services/proxmox/proxmox.nix
+++ NixModules/services/proxmox/proxmox.nix
@@ -8,10 +8,16 @@
   config = lib.mkIf config.nix-config.proxmox.enable {
     services.proxmox-ve = {
       enable = true;
-      ipAddress = "192.168.1.61";
+      ipAddress = "192.168.1.72";
     };
     networking.bridges.vmbr0.interfaces = [ "enp34s0" ];
-    networking.interfaces.vmbr0.useDHCP = lib.mkDefault true;
+    networking.interfaces.vmbr0.ipv4.addresses = [{
+      address = "192.168.1.72";
+      prefixLength = 16;
+    }];
+    networking.defaultGateway = "192.168.1.1";
+    networking.nameservers = [ "192.168.1.1" ];
+
     networking.firewall.allowedTCPPorts = [ 8006 ];
   };
 }
