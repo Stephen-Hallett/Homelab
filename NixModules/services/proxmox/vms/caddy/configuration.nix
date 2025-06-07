1599985be104e05734b8fd36b914bdcf40a80748
Stephen-Hallett <stevohallett@gmail.com>
2025-06-07 18:12:41 +1200

    feat: Add sops secret to caddy config

--- NixModules/services/proxmox/vms/caddy/configuration.nix
+++ NixModules/services/proxmox/vms/caddy/configuration.nix
@@ -23,12 +23,39 @@
 
   services.caddy = {
     enable = true;
+    user = "caddy";
     email = "stevohallett@gmail.com";
-    configFile = ./Caddyfile;
-    # environmentFile = "/run/secrets/caddy.env"; # Needs to be created manually in the VM
     package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
         hash = "sha256-Gsuo+ripJSgKSYOM9/yl6Kt/6BFCA6BuTDvPdteinAI=";
     };
+    extraConfig = ''
+      (cloudflare) {
+          tls {
+              dns cloudflare $(cat ${config.sops.secrets.cloudflare.path})
+          }
+      }
+
+      # proxmox
+      proxmox.hosted.stephenhallett.nz {
+          reverse_proxy https://100.76.206.4:8006 {
+              transport http {
+                  tls_insecure_skip_verify
+              }
+          }
+      }
+
+      # code-server
+      code.hosted.stephenhallett.nz {
+          reverse_proxy http://100.76.206.4:4200
+          import cloudflare
+      }
+
+      # qbittorrent
+      qbittorrent.hosted.stephenhallett.nz {
+          reverse_proxy http://192.168.1.76:8200
+          import cloudflare
+      }
+    '';
   };
 }
