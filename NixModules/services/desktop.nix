d59d21264080d39604a6702c75fe5f0e178425ca
Stephen-Hallett <stevohallett@gmail.com>
2025-07-20 14:13:27 +1200

    feat: Change to plasma5 instead of gnome

--- NixModules/services/desktop.nix
+++ NixModules/services/desktop.nix
@@ -9,7 +9,7 @@
 
     # Enable the GNOME Desktop Environment.
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
     environment.variables = {WAYLAND_DISPLAY = 0;};
 
     # Configure keymap in X11
