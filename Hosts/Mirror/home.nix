d6cddf1d6bc797907cece2dcd0b947996bba026a
Stephen-Hallett <stevohallett@gmail.com>
2025-12-01 21:24:54 +1300

    fix: Add systemd service to start unclutter on boot

--- Hosts/Mirror/home.nix
+++ Hosts/Mirror/home.nix
@@ -33,8 +33,9 @@
 
   services.unclutter = {
     enable = true;
-    timeout = 5;  # Hide cursor after 5 seconds of inactivity
-    threshold = 10;  # Minimum number of pixels cursor must move to be considered active
+    timeout = 5; # Hide cursor after 5 seconds of inactivity
+    threshold =
+      10; # Minimum number of pixels cursor must move to be considered active
   };
 
   fonts.fontconfig.enable = true;
@@ -52,30 +53,29 @@
     ripgrep.enable = true;
   };
 
-  systemd.user.services.mymirror = 
-    let
-      startScript = pkgs.writeShellScript "mymirror-start" ''
-        cd $HOME/myMirror/electron
-        ${pkgs.yarn}/bin/yarn install
-        exec ${pkgs.yarn}/bin/yarn start
-      '';
-    in {
-      Unit = {
-        Description = "myMirror Electron App";
-        After = [ "graphical-session.target" ];
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-      Service = {
-        Type = "simple";
-        Environment = [
-          "DISPLAY=:0"
-          "XAUTHORITY=%h/.Xauthority"
-        ];
-        ExecStart = "${startScript}";
-        Restart = "on-failure";
-        RestartSec = 10;
-      };
+  systemd.user.services.mymirror = let
+    startScript = pkgs.writeShellScript "mymirror-start" ''
+      cd $HOME/myMirror/electron
+      ${pkgs.yarn}/bin/yarn install
+      exec ${pkgs.yarn}/bin/yarn start
+    '';
+  in {
+    Unit = {
+      Description = "myMirror Electron App";
+      After = [ "graphical-session.target" ];
     };
+    Install = { WantedBy = [ "default.target" ]; };
+    Service = {
+      Type = "simple";
+      Environment = [ "DISPLAY=:0" "XAUTHORITY=%h/.Xauthority" ];
+      ExecStart = "${startScript}";
+      Restart = "on-failure";
+      RestartSec = 10;
+    };
+  };
+
+  systemd.user.services.unclutter = {
+    Unit = { After = [ "graphical-session.target" ]; };
+    Service = { Environment = [ "DISPLAY=:0" "XAUTHORITY=%h/.Xauthority" ]; };
+  };
 }
