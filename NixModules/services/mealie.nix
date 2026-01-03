f132a87ad5dad8766e32bc3e434408b2186541a2
Stephen-Hallett <stevohallett@gmail.com>
2026-01-03 15:19:00 +1300

    style: Change secondary colour

--- NixModules/services/mealie.nix
+++ NixModules/services/mealie.nix
@@ -23,7 +23,7 @@
         # Catppuccin Latte (Light Theme)
         THEME_LIGHT_PRIMARY = "#7287fd";    # Lavender (primary brand color)
         THEME_LIGHT_ACCENT = "#7287fd";     # Lavender (buttons/interactive)
-        THEME_LIGHT_SECONDARY = "#8839EF";  # Mauve (navigation/sidebar)
+        THEME_LIGHT_SECONDARY = "#04a5e5";  # Sky (navigation/sidebar)
         THEME_LIGHT_SUCCESS = "#40A02B";    # Green
         THEME_LIGHT_INFO = "#1E66F5";       # Blue
         THEME_LIGHT_WARNING = "#FE640B";    # Peach
@@ -32,7 +32,7 @@
         # Catppuccin Macchiato (Dark Theme)
         THEME_DARK_PRIMARY = "#b7bdf8";     # Lavender (primary brand color)
         THEME_DARK_ACCENT = "#b7bdf8";      # Lavender (buttons/interactive)
-        THEME_DARK_SECONDARY = "#C6A0F6";   # Mauve (navigation/sidebar)
+        THEME_DARK_SECONDARY = "#91d7e3";   # Sky (navigation/sidebar)
         THEME_DARK_SUCCESS = "#A6DA95";     # Green
         THEME_DARK_INFO = "#8AADF4";        # Blue
         THEME_DARK_WARNING = "#F5A97F";     # Peach
